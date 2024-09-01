2b56201765dc332674b8cb51e83eabf2aa44b1c2
Mike Voznesensky <mixask8@mail.ru>
2024-09-01 12:57:57 +0300

    Обноавлён файл init.el
    * Пока отключены все модули
    * Добавлена переменная минимальной версии Emacs
    * Удалена проверка SSL настроек для Emacs 26.2
    * Удалено исправление уязвимости для Emacs 25.3

--- init.el
+++ init.el
@@ -39,130 +39,157 @@
 ;; You may delete these explanatory comments.
                                         ;(package-initialize)
 
+;; Определение имя пользователя для Приветствия
+;; TODO проверить на Windows
 (defvar prelude-user
   (getenv
    (if (equal system-type 'windows-nt) "USERNAME" "USER")))
+;; Определяем минимальну версию Emacs на котором тестировался модуль
+(defvar prelude-emacs-required "29.4")
 
-(message "[Prelude] Prelude is powering up... Be patient, Master %s!" prelude-user)
+;; Приветствие
+(message "[Prelude] Prelude включается... Будьте терпеливее, Мастер %s!" prelude-user)
 
-(when (version< emacs-version "25.1")
-  (error "[Prelude] Prelude requires GNU Emacs 25.1 or newer, but you're running %s" emacs-version))
-(when (version<= emacs-version "26.2")
-  ;; require Gnutls >= 3.6
-  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
+;; Emacs не тестировался и не проверялся на версии ниже 29.4 поэтому что-то может не работать.
+(when (version< emacs-version prelude-emacs-required)
+  (warn "[Prelude] Prelude тестировалс на версии %s, но у тебя %s" prelude-emacs-required emacs-version))
 
 ;; Always load newest byte code
-(setq load-prefer-newer t)
+;; Не понятно зачем всё время загружать новый байт код?
+;; Видимо для тех случаев когда произошло обновление Prelude что бы код обновился.
+;; С такой опцией Prelude получается всегда дольше запускается в отличие от Spacemacs (не точно)
+;; Может быть нужно посмотреть в сторону нативной компиляции (https://www.gnu.org/software/emacs/manual/html_node/emacs/Lisp-Libraries.html)
+;; ???
+;; (setq load-prefer-newer t)
 
-;; Define Prelude's directory structure
-(defvar prelude-dir (file-name-directory load-file-name)
-  "The root dir of the Emacs Prelude distribution.")
-(defvar prelude-core-dir (expand-file-name "core" prelude-dir)
-  "The home of Prelude's core functionality.")
-(defvar prelude-modules-dir (expand-file-name  "modules" prelude-dir)
-  "This directory houses all of the built-in Prelude modules.")
-(defvar prelude-personal-dir (expand-file-name "personal" prelude-dir)
-  "This directory is for your personal configuration.
+;; Определяем структуру каталогов (пока всё вырезаем)
+;; TODO
+;; (defvar prelude-dir (file-name-directory load-file-name)
+;;   "The root dir of the Emacs Prelude distribution.")
+;; (defvar prelude-core-dir (expand-file-name "core" prelude-dir)
+;;   "The home of Prelude's core functionality.")
+;; (defvar prelude-modules-dir (expand-file-name  "modules" prelude-dir)
+;;   "This directory houses all of the built-in Prelude modules.")
+;; (defvar prelude-personal-dir (expand-file-name "personal" prelude-dir)
+;;   "This directory is for your personal configuration.
 
-Users of Emacs Prelude are encouraged to keep their personal configuration
-changes in this directory.  All Emacs Lisp files there are loaded automatically
-by Prelude.")
-(defvar prelude-personal-preload-dir (expand-file-name "preload" prelude-personal-dir)
-  "This directory is for your personal configuration, that you want loaded before Prelude.")
-(defvar prelude-vendor-dir (expand-file-name "vendor" prelude-dir)
-  "This directory houses packages that are not yet available in ELPA (or MELPA).")
-(defvar prelude-savefile-dir (expand-file-name "savefile" user-emacs-directory)
-  "This folder stores all the automatically generated save/history-files.")
-(defvar prelude-modules-file (expand-file-name "prelude-modules.el" prelude-personal-dir)
-  "This file contains a list of modules that will be loaded by Prelude.")
+;; Users of Emacs Prelude are encouraged to keep their personal configuration
+;; changes in this directory.  All Emacs Lisp files there are loaded automatically
+;; by Prelude.")
+;; (defvar prelude-personal-preload-dir (expand-file-name "preload" prelude-personal-dir)
+;;   "This directory is for your personal configuration, that you want loaded before Prelude.")
+;; (defvar prelude-vendor-dir (expand-file-name "vendor" prelude-dir)
+;;   "This directory houses packages that are not yet available in ELPA (or MELPA).")
+;; (defvar prelude-savefile-dir (expand-file-name "savefile" user-emacs-directory)
+;;   "This folder stores all the automatically generated save/history-files.")
+;; (defvar prelude-modules-file (expand-file-name "prelude-modules.el" prelude-personal-dir)
+;;   "This file contains a list of modules that will be loaded by Prelude.")
 
-(unless (file-exists-p prelude-savefile-dir)
-  (make-directory prelude-savefile-dir))
+;; Дальше идут функции которые объявлены в файлах Prelude
+;; Создание каталога для хранения истории модификации файла так как Emacs из коробки после закрытия сессии
+;; её больше не имеет.
+;; TODO
+;; (unless (file-exists-p prelude-savefile-dir)
+;;   (make-directory prelude-savefile-dir))
 
-(defun prelude-add-subfolders-to-load-path (parent-dir)
-  "Add all level PARENT-DIR subdirs to the `load-path'."
-  (dolist (f (directory-files parent-dir))
-    (let ((name (expand-file-name f parent-dir)))
-      (when (and (file-directory-p name)
-                 (not (string-prefix-p "." f)))
-        (add-to-list 'load-path name)
-        (prelude-add-subfolders-to-load-path name)))))
+;; Устанавливем путь загрузки модулей (наверное)
+;; TODO
+;; (defun prelude-add-subfolders-to-load-path (parent-dir)
+;;   "Add all level PARENT-DIR subdirs to the `load-path'."
+;;   (dolist (f (directory-files parent-dir))
+;;     (let ((name (expand-file-name f parent-dir)))
+;;       (when (and (file-directory-p name)
+;;                  (not (string-prefix-p "." f)))
+;;         (add-to-list 'load-path name)
+;;         (prelude-add-subfolders-to-load-path name)))))
 
-;; add Prelude's directories to Emacs's `load-path'
-(add-to-list 'load-path prelude-core-dir)
-(add-to-list 'load-path prelude-modules-dir)
-(add-to-list 'load-path prelude-vendor-dir)
-(prelude-add-subfolders-to-load-path prelude-vendor-dir)
+;; ;; add Prelude's directories to Emacs's `load-path'
+;; (add-to-list 'load-path prelude-core-dir)
+;; (add-to-list 'load-path prelude-modules-dir)
+;; (add-to-list 'load-path prelude-vendor-dir)
+;; (prelude-add-subfolders-to-load-path prelude-vendor-dir)
 
+;; Увеличивание размера буфера для сборщика мусора.
+;; Пока не понятно как он влияет на работу Emacs (https://www.gnu.org/software/emacs/manual/html_node/elisp/Garbage-Collection.html)
+;; TODO
 ;; reduce the frequency of garbage collection by making it happen on
 ;; each 50MB of allocated data (the default is on every 0.76MB)
-(setq gc-cons-threshold 50000000)
+;; (setq gc-cons-threshold 50000000)
 
-;; warn when opening files bigger than 100MB
-(setq large-file-warning-threshold 100000000)
-
+;; Установка персональных настроек
+;; ???
+;; TODO
 ;; preload the personal settings from `prelude-personal-preload-dir'
-(when (file-exists-p prelude-personal-preload-dir)
-  (message "[Prelude] Loading personal configuration files in %s..." prelude-personal-preload-dir)
-  (mapc 'load (directory-files prelude-personal-preload-dir 't "^[^#\.].*el$")))
+;; (when (file-exists-p prelude-personal-preload-dir)
+;;   (message "[Prelude] Loading personal configuration files in %s..." prelude-personal-preload-dir)
+;;   (mapc 'load (directory-files prelude-personal-preload-dir 't "^[^#\.].*el$")))
 
-(message "[Prelude] Loading Prelude's core modules...")
+;; (message "[Prelude] Loading Prelude's core modules...")
 
+;; Загрузка пакетов/модулей
+;; ???
+;; TODO
 ;; load the core stuff
-(require 'prelude-packages)
-(require 'prelude-custom)  ;; Needs to be loaded before core, editor and ui
-(require 'prelude-ui)
-(require 'prelude-core)
-(require 'prelude-mode)
-(require 'prelude-editor)
-(require 'prelude-global-keybindings)
+;; (require 'prelude-packages)
+;; (require 'prelude-custom)  ;; Needs to be loaded before core, editor and ui
+;; (require 'prelude-ui)
+;; (require 'prelude-core)
+;; (require 'prelude-mode)
+;; (require 'prelude-editor)
+;; (require 'prelude-global-keybindings)
 
-;; macOS specific settings
-(when (eq system-type 'darwin)
-  (require 'prelude-macos))
+;; Настройки для macOS
+;; Не тестировались
+;; TODO
+;; (when (eq system-type 'darwin)
+;;   (require 'prelude-macos))
 
-;; Linux specific settings
-(when (eq system-type 'gnu/linux)
-  (require 'prelude-linux))
+;; Настройки для GNU/Linux
+;; Не тестировались
+;; TODO
+;; (when (eq system-type 'gnu/linux)
+;;   (require 'prelude-linux))
 
-;; WSL specific setting
-(when (and (eq system-type 'gnu/linux) (getenv "WSLENV"))
-  (require 'prelude-wsl))
+;; Настройки для WSL
+;; Не тестировались
+;; TODO
+;; (when (and (eq system-type 'gnu/linux) (getenv "WSLENV"))
+;;   (require 'prelude-wsl))
 
-;; Windows specific settings
-(when (eq system-type 'windows-nt)
-  (require 'prelude-windows))
+;; Настройки для Windows
+;; Не тестировались
+;; TODO
+;; (when (eq system-type 'windows-nt)
+;;   (require 'prelude-windows))
 
-(message "[Prelude] Loading Prelude's additional modules...")
+;; (message "[Prelude] Loading Prelude's additional modules...")
 
+;; Настройка модулей ??? Что-то уже много настроек и приготовлений
+;; TODO
 ;; the modules
-(if (file-exists-p prelude-modules-file)
-    (load prelude-modules-file)
-  (message "[Prelude] Missing personal modules file %s" prelude-modules-file)
-  (message "[Prelude] Falling back to the bundled example file sample/prelude-modules.el")
-  (message "[Prelude] You should copy this file to your personal configuration folder and tweak it to your liking")
-  (load (expand-file-name "sample/prelude-modules.el" prelude-dir)))
+;; (if (file-exists-p prelude-modules-file)
+;;     (load prelude-modules-file)
+;;   (message "[Prelude] Missing personal modules file %s" prelude-modules-file)
+;;   (message "[Prelude] Falling back to the bundled example file sample/prelude-modules.el")
+;;   (message "[Prelude] You should copy this file to your personal configuration folder and tweak it to your liking")
+;;   (load (expand-file-name "sample/prelude-modules.el" prelude-dir)))
 
-;; config changes made through the customize UI will be stored here
-(setq custom-file (expand-file-name "custom.el" prelude-personal-dir))
+;; ;; config changes made through the customize UI will be stored here
+;; (setq custom-file (expand-file-name "custom.el" prelude-personal-dir))
 
-;; load the personal settings (this includes `custom-file')
-(when (file-exists-p prelude-personal-dir)
-  (message "[Prelude] Loading personal configuration files in %s..." prelude-personal-dir)
-  (mapc 'load (delete
-               prelude-modules-file
-               (directory-files prelude-personal-dir 't "^[^#\.].*\\.el$"))))
+;; ;; load the personal settings (this includes `custom-file')
+;; (when (file-exists-p prelude-personal-dir)
+;;   (message "[Prelude] Loading personal configuration files in %s..." prelude-personal-dir)
+;;   (mapc 'load (delete
+;;                prelude-modules-file
+;;                (directory-files prelude-personal-dir 't "^[^#\.].*\\.el$"))))
 
-(message "[Prelude] Prelude is ready to do thy bidding, Master %s!" prelude-user)
+;; (message "[Prelude] Prelude is ready to do thy bidding, Master %s!" prelude-user)
 
-;; Patch security vulnerability in Emacs versions older than 25.3
-(when (version< emacs-version "25.3")
-  (with-eval-after-load "enriched"
-    (defun enriched-decode-display-prop (start end &optional param)
-      (list start end))))
-
-(prelude-eval-after-init
- ;; greet the use with some useful tip
- (run-at-time 5 nil 'prelude-tip-of-the-day))
+;; Не понятно что происходит
+;; TODO
+;; (prelude-eval-after-init
+;;  ;; greet the use with some useful tip
+;;  (run-at-time 5 nil 'prelude-tip-of-the-day))
 
 ;;; init.el ends here
