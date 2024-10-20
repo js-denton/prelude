bf92c81a469e1d0313790eed1268b1edf82a2a2e
Mike Voznesensky <mixask8@mail.ru>
2024-10-20 20:41:56 +0300

    Обновлен файл prelude-custom.el
    * Удалёна неактуальная версия пакета в параметре
    * Закомментированы непонятные параметры
    * Добавлены дополнительные комментарии для будущего изучения
    * Удалена секция с темой оформления (используется нативная в prelude-ui)

--- core/prelude-custom.el
+++ core/prelude-custom.el
@@ -34,87 +34,91 @@
 
 ;;; Code:
 
-;; TODO remove package-version
-
 ;; customize
 (defgroup prelude nil
   "Параметры Prelude."
   :prefix "prelude-"
   :group 'convenience)
 
-(defcustom prelude-super-keybindings t
-  "Controls whether to use the Super key in keybindings.
-They can be problematic in some operating systems (e.g. Windows)
-or desktop environments that make heavy use of them."
-  :type 'boolean
-  :group 'prelude
-  :package-version '(prelude . "1.1"))
+;; ??? Неизвестно где используется и для чего
+;; (defcustom prelude-super-keybindings t
+;;   "Controls whether to use the Super key in keybindings.
+;; They can be problematic in some operating systems (e.g. Windows)
+;; or desktop environments that make heavy use of them."
+;;   :type 'boolean
+;;   :group 'prelude
 
+;; TODO необходимо разобраться как работает
 (defcustom prelude-auto-save t
   "Non-nil values enable Prelude's auto save."
   :type 'boolean
   :group 'prelude)
 
-(defcustom prelude-guru t
-  "Non-nil values enable `guru-mode'."
-  :type 'boolean
-  :group 'prelude)
+;; ??? Неизвестно что за режим
+;; (defcustom prelude-guru t
+;;   "Non-nil values enable `guru-mode'."
+;;   :type 'boolean
+;;   :group 'prelude)
 
+;; TODO узнать как работает, может быть полезно, но не через настроки,
+;; а по горячим клавишам
 (defcustom prelude-whitespace t
   "Non-nil values enable Prelude's whitespace visualization."
   :type 'boolean
   :group 'prelude)
 
+;; TODO как я пониамю это пробелы в конце строки - изучить
 (defcustom prelude-clean-whitespace-on-save t
   "Cleanup whitespace from file before it's saved.
 Will only occur if `prelude-whitespace' is also enabled."
   :type 'boolean
   :group 'prelude)
 
+;; TODO Это проверка орфографии. Переделать на hunspell - для поддержки двух языков.
 (defcustom prelude-flyspell t
   "Non-nil values enable Prelude's flyspell support."
   :type 'boolean
   :group 'prelude)
 
-(defcustom prelude-user-init-file (expand-file-name "personal/"
-                                                    user-emacs-directory)
-  "Path to your personal customization file.
-Prelude recommends you only put personal customizations in the
-personal folder.  This variable allows you to specify a specific
-folder as the one that should be visited when running
-`crux-find-user-init-file'.  This can be easily set to the desired buffer
-in Lisp by putting `(setq prelude-user-init-file load-file-name)'
-in the desired elisp file."
-  :type 'string
-  :group 'prelude)
+;; ??? Персональный каталог настроек уже определён в init.py, это можно опустить.
+;; (defcustom prelude-user-init-file (expand-file-name "personal/"
+;;                                                     user-emacs-directory)
+;;   "Path to your personal customization file.
+;; Prelude recommends you only put personal customizations in the
+;; personal folder.  This variable allows you to specify a specific
+;; folder as the one that should be visited when running
+;; `crux-find-user-init-file'.  This can be easily set to the desired buffer
+;; in Lisp by putting `(setq prelude-user-init-file load-file-name)'
+;; in the desired elisp file."
+;;   :type 'string
+;;   :group 'prelude)
 
-(defcustom prelude-indent-sensitive-modes
-  '(conf-mode coffee-mode haml-mode python-mode slim-mode yaml-mode)
-  "Modes for which auto-indenting is suppressed."
-  :type 'list
-  :group 'prelude)
+;; ??? Можно опустить. Подавление автоматического отступа должна быть на уровне режима редактирования.
+;; (defcustom prelude-indent-sensitive-modes
+;;   '(conf-mode coffee-mode haml-mode python-mode slim-mode yaml-mode)
+;;   "Modes for which auto-indenting is suppressed."
+;;   :type 'list
+;;   :group 'prelude)
 
-(defcustom prelude-format-on-save t
-  "Run mode specific format on file before it's saved.
-Currently only applies to tide-mode."
-  :type 'boolean
-  :group 'prelude)
+;; ??? не понятно какие задачи это решает
+;; (defcustom prelude-format-on-save t
+;;   "Run mode specific format on file before it's saved.
+;; Currently only applies to tide-mode."
+;;   :type 'boolean
+;;   :group 'prelude)
 
-(defcustom prelude-yank-indent-modes '(LaTeX-mode TeX-mode)
-  "Modes in which to indent regions that are yanked (or yank-popped).
-Only modes that don't derive from `prog-mode' should be listed here."
-  :type 'list
-  :group 'prelude)
+;; ??? Специфичный мод который делает что-то с отсупами при копировании
+;; (defcustom prelude-yank-indent-modes '(LaTeX-mode TeX-mode)
+;;   "Modes in which to indent regions that are yanked (or yank-popped).
+;; Only modes that don't derive from `prog-mode' should be listed here."
+;;   :type 'list
+;;   :group 'prelude)
 
-(defcustom prelude-yank-indent-threshold 1000
-  "Threshold (# chars) over which indentation does not automatically occur."
-  :type 'number
-  :group 'prelude)
-
-(defcustom prelude-theme 'zenburn
-  "The default color theme, change this in your /personal/preload config."
-  :type 'symbol
-  :group 'prelude)
+;; ??? Что-то тоже с отступами
+;; (defcustom prelude-yank-indent-threshold 1000
+;;   "Threshold (# chars) over which indentation does not automatically occur."
+;;   :type 'number
+;;   :group 'prelude)
 
 (provide 'prelude-custom)
 
