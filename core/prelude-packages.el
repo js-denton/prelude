bb3518f60a58f9bc03fcbaeaedff7379bec449e3
Mike Voznesensky <mixask8@mail.ru>
2025-01-06 16:34:00 +0300

    Заменено использование package.el на straight
    * Добавлена зависимость magit

--- core/prelude-packages.el
+++ core/prelude-packages.el
@@ -31,204 +31,223 @@
 ;; Boston, MA 02110-1301, USA.
 
 ;;; Code:
-(require 'cl-lib)
-(require 'package)
+;; Инициализация straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-;;;; Package setup and additional utility functions
+(straight-use-package 'magit)
 
-;; accessing a package repo over https on Windows is a no go, so we
-;; fallback to http there
-(if (and (>= emacs-major-version 27) (>= emacs-minor-version 1))
-  (if (eq system-type 'windows-nt)
-      (add-to-list 'package-archives
-                   '("melpa" . "http://melpa.org/packages/") t)
-    (add-to-list 'package-archives
-                 '("melpa" . "https://melpa.org/packages/") t)))
+;; (require 'cl-lib)
+;; (require 'package)
 
-;; load the pinned packages
-(let ((prelude-pinned-packages-file (expand-file-name "prelude-pinned-packages.el" prelude-dir)))
-  (if (file-exists-p prelude-pinned-packages-file)
-      (load prelude-pinned-packages-file)))
+;; ;;;; Package setup and additional utility functions
 
-;; set package-user-dir to be relative to Prelude install path
-(setq package-user-dir (expand-file-name "elpa" prelude-dir))
-(package-initialize)
+;; ;; accessing a package repo over https on Windows is a no go, so we
+;; ;; fallback to http there
+;; (if (and (>= emacs-major-version 27) (>= emacs-minor-version 1))
+;;   (if (eq system-type 'windows-nt)
+;;       (add-to-list 'package-archives
+;;                    '("melpa" . "http://melpa.org/packages/") t)
+;;     (add-to-list 'package-archives
+;;                  '("melpa" . "https://melpa.org/packages/") t)))
 
-;; install & enable use-package
-(unless (package-installed-p 'use-package)
-  ;; emacs 26.1 can't install package use-package if not do refresh
-  (package-refresh-contents)
-  (package-install 'use-package))
+;; ;; load the pinned packages
+;; (let ((prelude-pinned-packages-file (expand-file-name "prelude-pinned-packages.el" prelude-dir)))
+;;   (if (file-exists-p prelude-pinned-packages-file)
+;;       (load prelude-pinned-packages-file)))
 
-(require 'use-package)
-(setq use-package-verbose t)
+;; ;; set package-user-dir to be relative to Prelude install path
+;; (setq package-user-dir (expand-file-name "elpa" prelude-dir))
+;; (package-initialize)
 
-(defvar prelude-packages
-  '(ace-window
-    ag
-    avy
-    anzu
-    browse-kill-ring
-    crux
-    discover-my-major
-    diff-hl
-    diminish
-    easy-kill
-    editorconfig
-    epl
-    expand-region
-    flycheck
-    gist
-    git-timemachine
-    git-modes
-    guru-mode
-    hl-todo
-    imenu-anywhere
-    projectile
-    magit
-    move-text
-    nlinum
-    operate-on-number
-    smartparens
-    smartrep
-    super-save
-    undo-tree
-    volatile-highlights
-    which-key
-    zenburn-theme
-    zop-to-char)
-  "A list of packages to ensure are installed at launch.")
+;; ;; install & enable use-package
+;; (unless (package-installed-p 'use-package)
+;;   ;; emacs 26.1 can't install package use-package if not do refresh
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
 
-(defun prelude-packages-installed-p ()
-  "Check if all packages in `prelude-packages' are installed."
-  (cl-every #'package-installed-p prelude-packages))
+;; (require 'use-package)
+;; (setq use-package-verbose t)
 
-(defun prelude-require-package (package)
-  "Install PACKAGE unless already installed."
-  (unless (memq package prelude-packages)
-    (add-to-list 'prelude-packages package))
-  (unless (package-installed-p package)
-    (package-install package)))
+;; (defvar prelude-packages
+;;   '(ace-window
+;;     ag
+;;     avy
+;;     anzu
+;;     browse-kill-ring
+;;     crux
+;;     discover-my-major
+;;     diff-hl
+;;     diminish
+;;     easy-kill
+;;     editorconfig
+;;     epl
+;;     expand-region
+;;     flycheck
+;;     gist
+;;     git-timemachine
+;;     git-modes
+;;     guru-mode
+;;     hl-todo
+;;     imenu-anywhere
+;;     projectile
+;;     magit
+;;     move-text
+;;     nlinum
+;;     operate-on-number
+;;     smartparens
+;;     smartrep
+;;     super-save
+;;     undo-tree
+;;     volatile-highlights
+;;     which-key
+;;     zenburn-theme
+;;     zop-to-char)
+;;   "A list of packages to ensure are installed at launch.")
 
-(defun prelude-require-packages (packages)
-  "Ensure PACKAGES are installed.
-Missing packages are installed automatically."
-  (mapc #'prelude-require-package packages))
+;; (defun prelude-packages-installed-p ()
+;;   "Check if all packages in `prelude-packages' are installed."
+;;   (cl-every #'package-installed-p prelude-packages))
 
-(defun prelude-install-packages ()
-  "Install all packages listed in `prelude-packages'."
-  (unless (prelude-packages-installed-p)
-    ;; check for new packages (package versions)
-    (message "%s" "Emacs Prelude is now refreshing its package database...")
-    (package-refresh-contents)
-    (message "%s" " done.")
-    ;; install the missing packages
-    (prelude-require-packages prelude-packages)))
+;; (defun prelude-require-package (package)
+;;   "Install PACKAGE unless already installed."
+;;   (unless (memq package prelude-packages)
+;;     (add-to-list 'prelude-packages package))
+;;   (unless (package-installed-p package)
+;;     (package-install package)))
 
-;; run package installation
-(prelude-install-packages)
+;; (defun prelude-require-packages (packages)
+;;   "Ensure PACKAGES are installed.
+;; Missing packages are installed automatically."
+;;   (mapc #'prelude-require-package packages))
 
-(defun prelude-list-foreign-packages ()
-  "Browse third-party packages not bundled with Prelude.
+;; (defun prelude-install-packages ()
+;;   "Install all packages listed in `prelude-packages'."
+;;   (unless (prelude-packages-installed-p)
+;;     ;; check for new packages (package versions)
+;;     (message "%s" "Emacs Prelude is now refreshing its package database...")
+;;     (package-refresh-contents)
+;;     (message "%s" " done.")
+;;     ;; install the missing packages
+;;     (prelude-require-packages prelude-packages)))
 
-Behaves similarly to `package-list-packages', but shows only the packages that
-are installed and are not in `prelude-packages'.  Useful for
-removing unwanted packages."
-  (interactive)
-  (package-show-package-list
-   (cl-set-difference package-activated-list prelude-packages)))
+;; ;; run package installation
+;; (prelude-install-packages)
 
-;;;; Auto-installation of major modes on demand
+;; (defun prelude-list-foreign-packages ()
+;;   "Browse third-party packages not bundled with Prelude.
 
-(defmacro prelude-auto-install (extension package mode)
-  "When file with EXTENSION is opened triggers auto-install of PACKAGE.
-PACKAGE is installed only if not already present.  The file is opened in MODE."
-  `(add-to-list 'auto-mode-alist
-                `(,extension . (lambda ()
-                                 (unless (package-installed-p ',package)
-                                   (package-install ',package))
-                                 (,mode)))))
+;; Behaves similarly to `package-list-packages', but shows only the packages that
+;; are installed and are not in `prelude-packages'.  Useful for
+;; removing unwanted packages."
+;;   (interactive)
+;;   (package-show-package-list
+;;    (cl-set-difference package-activated-list prelude-packages)))
 
-(defvar prelude-auto-install-alist
-  '(("\\.adoc\\'" adoc-mode adoc-mode)
-    ("\\.clj\\'" clojure-mode clojure-mode)
-    ("\\.cljc\\'" clojure-mode clojurec-mode)
-    ("\\.cljs\\'" clojure-mode clojurescript-mode)
-    ("\\.edn\\'" clojure-mode clojure-mode)
-    ("\\.cmake\\'" cmake-mode cmake-mode)
-    ("CMakeLists\\.txt\\'" cmake-mode cmake-mode)
-    ("\\.coffee\\'" coffee-mode coffee-mode)
-    ("\\.css\\'" css-mode css-mode)
-    ("\\.csv\\'" csv-mode csv-mode)
-    ("Cask" cask-mode cask-mode)
-    ("\\.d\\'" d-mode d-mode)
-    ("\\.dart\\'" dart-mode dart-mode)
-    ("\\.elm\\'" elm-mode elm-mode)
-    ("\\.ex\\'" elixir-mode elixir-mode)
-    ("\\.exs\\'" elixir-mode elixir-mode)
-    ("\\.elixir\\'" elixir-mode elixir-mode)
-    ("\\.erl\\'" erlang erlang-mode)
-    ("\\.feature\\'" feature-mode feature-mode)
-    ("\\.go\\'" go-mode go-mode)
-    ("\\.graphql\\'" graphql-mode graphql-mode)
-    ("\\.groovy\\'" groovy-mode groovy-mode)
-    ("\\.haml\\'" haml-mode haml-mode)
-    ("\\.hs\\'" haskell-mode haskell-mode)
-    ("\\.jl\\'" julia-mode julia-mode)
-    ("\\.json\\'" json-mode json-mode)
-    ("\\.kt\\'" kotlin-mode kotlin-mode)
-    ("\\.kv\\'" kivy-mode kivy-mode)
-    ("\\.latex\\'" auctex LaTeX-mode)
-    ("\\.less\\'" less-css-mode less-css-mode)
-    ("\\.lua\\'" lua-mode lua-mode)
-    ("\\.markdown\\'" markdown-mode markdown-mode)
-    ("\\.md\\'" markdown-mode markdown-mode)
-    ("\\.ml\\'" tuareg tuareg-mode)
-    ("\\.pp\\'" puppet-mode puppet-mode)
-    ("\\.php\\'" php-mode php-mode)
-    ("\\.proto\\'" protobuf-mode protobuf-mode)
-    ("\\.pyd\\'" cython-mode cython-mode)
-    ("\\.pyi\\'" cython-mode cython-mode)
-    ("\\.pyx\\'" cython-mode cython-mode)
-    ("PKGBUILD\\'" pkgbuild-mode pkgbuild-mode)
-    ("\\.rkt\\'" racket-mode racket-mode)
-    ("\\.rs\\'" rust-mode rust-mode)
-    ("\\.sass\\'" sass-mode sass-mode)
-    ("\\.scala\\'" scala-mode scala-mode)
-    ("\\.scss\\'" scss-mode scss-mode)
-    ("\\.slim\\'" slim-mode slim-mode)
-    ("\\.styl\\'" stylus-mode stylus-mode)
-    ("\\.swift\\'" swift-mode swift-mode)
-    ("\\.textile\\'" textile-mode textile-mode)
-    ("\\.thrift\\'" thrift thrift-mode)
-    ("\\.yml\\'" yaml-mode yaml-mode)
-    ("\\.yaml\\'" yaml-mode yaml-mode)
-    ("Dockerfile\\'" dockerfile-mode dockerfile-mode)))
+;; ;;;; Auto-installation of major modes on demand
 
-;; markdown-mode doesn't have autoloads for the auto-mode-alist
-;; so we add them manually if it's already installed
-(when (package-installed-p 'markdown-mode)
-  (add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
-  (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode)))
+;; (defmacro prelude-auto-install (extension package mode)
+;;   "When file with EXTENSION is opened triggers auto-install of PACKAGE.
+;; PACKAGE is installed only if not already present.  The file is opened in MODE."
+;;   `(add-to-list 'auto-mode-alist
+;;                 `(,extension . (lambda ()
+;;                                  (unless (package-installed-p ',package)
+;;                                    (package-install ',package))
+;;                                  (,mode)))))
 
-;; same with adoc-mode
-(when (package-installed-p 'adoc-mode)
-  (add-to-list 'auto-mode-alist '("\\.adoc\\'" . adoc-mode))
-  (add-to-list 'auto-mode-alist '("\\.asciidoc\\'" . adoc-mode)))
+;; (defvar prelude-auto-install-alist
+;;   '(("\\.adoc\\'" adoc-mode adoc-mode)
+;;     ("\\.clj\\'" clojure-mode clojure-mode)
+;;     ("\\.cljc\\'" clojure-mode clojurec-mode)
+;;     ("\\.cljs\\'" clojure-mode clojurescript-mode)
+;;     ("\\.edn\\'" clojure-mode clojure-mode)
+;;     ("\\.cmake\\'" cmake-mode cmake-mode)
+;;     ("CMakeLists\\.txt\\'" cmake-mode cmake-mode)
+;;     ("\\.coffee\\'" coffee-mode coffee-mode)
+;;     ("\\.css\\'" css-mode css-mode)
+;;     ("\\.csv\\'" csv-mode csv-mode)
+;;     ("Cask" cask-mode cask-mode)
+;;     ("\\.d\\'" d-mode d-mode)
+;;     ("\\.dart\\'" dart-mode dart-mode)
+;;     ("\\.elm\\'" elm-mode elm-mode)
+;;     ("\\.ex\\'" elixir-mode elixir-mode)
+;;     ("\\.exs\\'" elixir-mode elixir-mode)
+;;     ("\\.elixir\\'" elixir-mode elixir-mode)
+;;     ("\\.erl\\'" erlang erlang-mode)
+;;     ("\\.feature\\'" feature-mode feature-mode)
+;;     ("\\.go\\'" go-mode go-mode)
+;;     ("\\.graphql\\'" graphql-mode graphql-mode)
+;;     ("\\.groovy\\'" groovy-mode groovy-mode)
+;;     ("\\.haml\\'" haml-mode haml-mode)
+;;     ("\\.hs\\'" haskell-mode haskell-mode)
+;;     ("\\.jl\\'" julia-mode julia-mode)
+;;     ("\\.json\\'" json-mode json-mode)
+;;     ("\\.kt\\'" kotlin-mode kotlin-mode)
+;;     ("\\.kv\\'" kivy-mode kivy-mode)
+;;     ("\\.latex\\'" auctex LaTeX-mode)
+;;     ("\\.less\\'" less-css-mode less-css-mode)
+;;     ("\\.lua\\'" lua-mode lua-mode)
+;;     ("\\.markdown\\'" markdown-mode markdown-mode)
+;;     ("\\.md\\'" markdown-mode markdown-mode)
+;;     ("\\.ml\\'" tuareg tuareg-mode)
+;;     ("\\.pp\\'" puppet-mode puppet-mode)
+;;     ("\\.php\\'" php-mode php-mode)
+;;     ("\\.proto\\'" protobuf-mode protobuf-mode)
+;;     ("\\.pyd\\'" cython-mode cython-mode)
+;;     ("\\.pyi\\'" cython-mode cython-mode)
+;;     ("\\.pyx\\'" cython-mode cython-mode)
+;;     ("PKGBUILD\\'" pkgbuild-mode pkgbuild-mode)
+;;     ("\\.rkt\\'" racket-mode racket-mode)
+;;     ("\\.rs\\'" rust-mode rust-mode)
+;;     ("\\.sass\\'" sass-mode sass-mode)
+;;     ("\\.scala\\'" scala-mode scala-mode)
+;;     ("\\.scss\\'" scss-mode scss-mode)
+;;     ("\\.slim\\'" slim-mode slim-mode)
+;;     ("\\.styl\\'" stylus-mode stylus-mode)
+;;     ("\\.swift\\'" swift-mode swift-mode)
+;;     ("\\.textile\\'" textile-mode textile-mode)
+;;     ("\\.thrift\\'" thrift thrift-mode)
+;;     ("\\.yml\\'" yaml-mode yaml-mode)
+;;     ("\\.yaml\\'" yaml-mode yaml-mode)
+;;     ("Dockerfile\\'" dockerfile-mode dockerfile-mode)))
 
-;; and pkgbuild-mode
-(when (package-installed-p 'pkgbuild-mode)
-  (add-to-list 'auto-mode-alist '("PKGBUILD\\'" . pkgbuild-mode)))
+;; ;; markdown-mode doesn't have autoloads for the auto-mode-alist
+;; ;; so we add them manually if it's already installed
+;; (when (package-installed-p 'markdown-mode)
+;;   (add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
+;;   (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode)))
 
-;; build auto-install mappings
-(mapc
- (lambda (entry)
-   (let ((extension (car entry))
-         (package (cadr entry))
-         (mode (cadr (cdr entry))))
-     (unless (package-installed-p package)
-       (prelude-auto-install extension package mode))))
- prelude-auto-install-alist)
+;; ;; same with adoc-mode
+;; (when (package-installed-p 'adoc-mode)
+;;   (add-to-list 'auto-mode-alist '("\\.adoc\\'" . adoc-mode))
+;;   (add-to-list 'auto-mode-alist '("\\.asciidoc\\'" . adoc-mode)))
+
+;; ;; and pkgbuild-mode
+;; (when (package-installed-p 'pkgbuild-mode)
+;;   (add-to-list 'auto-mode-alist '("PKGBUILD\\'" . pkgbuild-mode)))
+
+;; ;; build auto-install mappings
+;; (mapc
+;;  (lambda (entry)
+;;    (let ((extension (car entry))
+;;          (package (cadr entry))
+;;          (mode (cadr (cdr entry))))
+;;      (unless (package-installed-p package)
+;;        (prelude-auto-install extension package mode))))
+;;  prelude-auto-install-alist)
 
 (provide 'prelude-packages)
 ;; Local Variables:
