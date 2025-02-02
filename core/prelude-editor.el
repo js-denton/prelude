6993d39cc6c41643ffb098eca75084a9218ce3bd
Mike Voznesensky <mixask8@mail.ru>
2025-02-02 19:31:43 +0300

    Удалены настройки символа табуляции
    Для Emacs 29 нет необходимости модифицировать tab.
    Для автодополнения слов есть специальная команда M-/

--- core/prelude-editor.el
+++ core/prelude-editor.el
@@ -30,18 +30,6 @@
 
 ;;; Code:
 
-;; Death to the tabs!  However, tabs historically indent to the next
-;; 8-character offset; specifying anything else will cause *mass*
-;; confusion, as it will change the appearance of every existing file.
-;; In some cases (python), even worse -- it will change the semantics
-;; (meaning) of the program.
-;;
-;; Emacs modes typically provide a standard means to change the
-;; indentation width -- eg. c-basic-offset: use that to adjust your
-;; personal indentation width, while maintaining the style (and
-;; meaning) of any files you load.
-(setq-default indent-tabs-mode nil)   ;; don't use tabs to indent
-(setq-default tab-width 8)            ;; but maintain correct appearance
 
 ;; Newline at end of file
 (setq require-final-newline t)
@@ -69,9 +57,6 @@
                                          try-expand-line
                                          try-complete-lisp-symbol-partially
                                          try-complete-lisp-symbol))
-
-;; smart tab behavior - indent or complete
-(setq tab-always-indent 'complete)
 
 ;; smart pairing for all
 (require 'smartparens-config)
