60b39b74d0c105d540c63047a9ea8cc9ef1f1cbd
Mike Voznesensky <mixask8@mail.ru>
2025-10-29 17:12:48 +0300

    В списке доступных пресетов доступен только org-mode
    Всё остальное заблокировано то тех пор пока не будет изучено.

--- sample/prelude-modules.el
+++ sample/prelude-modules.el
@@ -42,13 +42,14 @@
 ;;; General productivity tools
 
 ;; (require 'prelude-ido) ;; Supercharges Emacs completion for C-x C-f and more
-(if (version< emacs-version "27.1")
-    (require 'prelude-ivy) ;; A mighty modern alternative to ido
-  (require 'prelude-vertico)) ;; A powerful, yet simple, alternative to ivy
+;; TODO разобратьсяс vertico, возможно стоит перенести настройки сюда.
+;; (if (version< emacs-version "27.1")
+;;     (require 'prelude-ivy) ;; A mighty modern alternative to ido
+;;   (require 'prelude-vertico)) ;; A powerful, yet simple, alternative to ivy
 
 ;; (require 'prelude-helm) ;; Interface for narrowing and search
 ;; (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
-(require 'prelude-company)
+;; (require 'prelude-company)
 ;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
 
 ;;; Vim emulation
@@ -56,9 +57,9 @@
 ;; Enable this module if you're fond of vim's keybindings.
 ;; (require 'prelude-evil)
 
-;;; Org-mode (a legendary productivity tool that deserves its own category)
+;;; Org-mode (легендарный инструмент повышения производительности и заслуживающий отдельного модуля)
 ;;
-;; Org-mode helps you keep TODO lists, notes and more.
+;; Org-mode помогает вам вести списки дел, заметки и многое другое.
 (require 'prelude-org)
 
 ;;; Programming languages support
@@ -66,41 +67,41 @@
 ;; Modules for a few very common programming languages
 ;; are enabled by default.
 
-(require 'prelude-c)
+;; (require 'prelude-c)
 ;; (require 'prelude-clojure)
 ;; (require 'prelude-coffee)
 ;; (require 'prelude-common-lisp)
-(require 'prelude-css)
+;; (require 'prelude-css)
 ;; (require 'prelude-dart)
-(require 'prelude-emacs-lisp)
+;; (require 'prelude-emacs-lisp)
 ;; (require 'prelude-erlang)
 ;; (require 'prelude-elixir)
 ;; (require 'prelude-fsharp)
 ;; (require 'prelude-go)
 ;; (require 'prelude-haskell)
-(require 'prelude-js)
+;; (require 'prelude-js)
 ;; (require 'prelude-latex)
-(require 'prelude-lisp) ;; Common setup for Lisp-like languages
+;; (require 'prelude-lisp) ;; Common setup for Lisp-like languages
 ;; (require 'prelude-literate-programming) ;; Setup for Literate Programming
-(require 'prelude-lsp) ;; Base setup for the Language Server Protocol
+;; (require 'prelude-lsp) ;; Base setup for the Language Server Protocol
 ;; (require 'prelude-lua)
 ;; (require 'prelude-ocaml)
-(require 'prelude-perl)
+;; (require 'prelude-perl)
 ;; (require 'prelude-python)
 ;; (require 'prelude-racket)
 ;; (require 'prelude-ruby)
 ;; (require 'prelude-rust)
 ;; (require 'prelude-scala)
 ;; (require 'prelude-scheme)
-(require 'prelude-shell)
+;; (require 'prelude-shell)
 ;; (require 'prelude-scss)
 ;; (require 'prelude-ts)
-(require 'prelude-web) ;; Emacs mode for web templates
-(require 'prelude-xml)
-(require 'prelude-yaml)
+;; (require 'prelude-web) ;; Emacs mode for web templates
+;; (require 'prelude-xml)
+;; (require 'prelude-yaml)
 
 ;;; Misc
-(require 'prelude-erc) ;; A popular Emacs IRC client (useful if you're still into Freenode)
+;; (require 'prelude-erc) ;; A popular Emacs IRC client (useful if you're still into Freenode)
 
 (provide 'prelude-modules)
 ;;; prelude-modules.el ends here
